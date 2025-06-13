a450160779b00a8f5d0f4c6a8be33d99238dc841
Avinash <avinash592004@gmail.com>
2025-06-14 00:37:36 +0530

    done

---
+++ Binary_search[1D,2D,searchSpace]/bsforX.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int arr[]={-1,0,3,5,9,12};
+    int target = -1;
+
+    int start=0;
+    int end=(sizeof(arr) / sizeof(arr[0]))-1;
+
+    while(start<=end){
+        int mid=(start+end)/2;
+        if(arr[mid]==target){
+            cout << mid << ":" << arr[mid];
+            return 0;
+        }
+        if(target<arr[mid]){
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    cout << "-1" << endl;
+}
