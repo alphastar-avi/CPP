ceb4155871277d0ab1b898388419d86283b91ad1
Avinash <avinash592004@gmail.com>
2025-07-23 00:41:33 +0530

    160. Intersection of Two Linked Lists.

--- Linked_list/160_IntersectionofTwoLinkedLists.cpp
+++ Linked_list/160_IntersectionofTwoLinkedLists.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* l1 = headA;
+        ListNode* l2 = headB;
+
+        while(l1 != l2){
+            l1 = l1->next;
+            l2 = l2->next;
+
+            if(l1==l2) return l1;
+            if(!l1) l1 = headB;
+            if(!l2) l2 = headA;
+        }
+        return l1;
+    }
+};
+
+
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         map<ListNode*, int> mpp;
         ListNode* HA = headA;
         ListNode* HB = headB;
