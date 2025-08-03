dcf7d84b73b7f1fde15cca13b92434a778bc6a3d
Avinash <avinash592004@gmail.com>
2025-08-03 17:16:55 +0530

    678. Valid Parenthesis String.

--- Strings/678_ValidParenthesisString.cpp
+++ Strings/678_ValidParenthesisString.cpp
@@ -12,3 +12,25 @@ public:
         return low == 0;
     }
 };
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0, high = 0;
+        for (char c : s) {
+            if (c == '(') {
+                low++;
+                high++;
+            } else if (c == ')') {
+                low--;
+                high--;
+            } else { // '*'
+                low--;   // assume '*' is ')'
+                high++;  // assume '*' is '('
+            }
+            if (high < 0) return false; // too many ')'
+            if (low < 0) low = 0;       // can't go negative
+        }
+        return low == 0;
+    }
+};
