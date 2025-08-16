ef4d2c5c305b35aaecb65378289b2be5258dd714
Avinash <avinash592004@gmail.com>
2025-08-16 19:47:18 +0530

    930. Binary Subarrays With Sum

---
+++ Step10SlidingWindow&TwoPointerCombinedProblems/930_BinarySubarraysWithSum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subArrays(vector<int>& nums, int goal) {
+        if(goal<0) return 0;
+        int sum = 0, count = 0, left = 0;
+        for(int right = 0; right < nums.size(); right++){
+            sum = sum + nums[right];
+            while(sum > goal){
+                sum -= nums[left];
+                left++;
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return subArrays(nums, goal) - subArrays(nums, goal-1);
+    }
+};
