b5fd7fec61b86b38bf0010c73cf7ee9d79c45c7e
Avinash <avinash592004@gmail.com>
2025-08-17 18:34:30 +0530

    version 2

--- SlidingWindow&TwoPointerCombinedProblems/1358_NumberofSubstringsContainingAllThreeCharacters.cpp
+++ SlidingWindow&TwoPointerCombinedProblems/1358_NumberofSubstringsContainingAllThreeCharacters.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+       int lastseen[3]={-1,-1,-1}, count=0;
+       for(int i=0; i<s.size(); i++){
+        lastseen[s[i]-'a']=i;
+        if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){
+            int minLast = min({lastseen[0],lastseen[1],lastseen[2]});
+            count += (1 + minLast);
+        }
+       }
+       return count;
+
+    }
+};
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
         int count=0;
         for(int i=0; i<s.size(); i++){
             int arr[3]={0};
