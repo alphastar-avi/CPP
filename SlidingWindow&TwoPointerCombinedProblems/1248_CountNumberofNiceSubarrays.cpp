f7d7e9d5c1d35c8609866cc2f463010ebb33ed04
Avinash <avinash592004@gmail.com>
2025-08-17 19:57:31 +0530

    1248. Count Number of Nice Subarrays

--- SlidingWindow&TwoPointerCombinedProblems/1248_CountNumberofNiceSubarrays.cpp
+++ SlidingWindow&TwoPointerCombinedProblems/1248_CountNumberofNiceSubarrays.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int atMost(vector<int>& nums, int k) {
+        int left = 0, count = 0, odds = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] % 2) odds++; // count odd numbers
+
+            while (odds > k) { // shrink if too many odds
+                if (nums[left] % 2) odds--;
+                left++;
+            }
+            count += (right - left + 1); // all subarrays ending at right
+        }
+        return count;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+};
+
+class Solution {
+public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         for(int &it : nums){
             if(it%2==0){
@@ -21,3 +42,5 @@ public:
 
     }
 };
+
+
