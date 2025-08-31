4f1faaf486f29bb46ae96dc1a7d79fb9baa8fc17
Avinash <avinash592004@gmail.com>
2025-08-31 23:44:07 +0530

    390. Elimination Game.

--- recursion/390_EliminationGame.cpp
+++ recursion/390_EliminationGame.cpp
@@ -16,3 +16,24 @@ public:
         else return 2 * f(n / 2);
     }
 };
+
+// non recrusive
+// class Solution {
+// public:
+//     int lastRemaining(int n) {
+//         int head = 1;
+//         int step = 1;
+//         int remaining = n;
+//         bool left = true;
+        
+//         while (remaining > 1) {
+//             if (left || (remaining % 2 == 1)) {
+//                 head += step;
+//             }
+//             remaining /= 2;
+//             step *= 2;
+//             left = !left;
+//         }
+//         return head;
+//     }
+// };
