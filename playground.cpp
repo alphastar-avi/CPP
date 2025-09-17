f9d830212a76b59d7d68a522e997e2bba5aee809
Avinash <avinash592004@gmail.com>
2025-09-17 22:58:00 +0530

    ;

--- playground.cpp
+++ playground.cpp
@@ -1,36 +1,48 @@
-// #include<iostream>
-// #include<vector>
-// using namespace std;
+// // #include<iostream>
+// // #include<vector>
+// // using namespace std;
 
-// void print(int i, int n){
-//     cout << i << endl;
-//     if(i==n) {
-//         return ;
-//     }
+// // void print(int i, int n){
+// //     cout << i << endl;
+// //     if(i==n) {
+// //         return ;
+// //     }
 
-// print(i+1,n);
+// // print(i+1,n);
 
+// // }
+
+// // int main(){
+// //     int x;
+// // cin >> x;
+// // print(1,x);
+
+
+// // }
+// #include <stdio.h>
+// #include <time.h>
+
+// int main() {
+//     clock_t start = clock();
+
+//     // Some heavy work
+//     for (long long i = 0; i < 100000000; i++);
+
+//     clock_t end = clock();
+//     double cpu_time = (double)(end - start) / CLOCKS_PER_SEC;
+
+//     printf("CPU time used: %f seconds\n", cpu_time);
+//     return 0;
 // }
 
-// int main(){
-//     int x;
-// cin >> x;
-// print(1,x);
+using namespace std;
 
-
-// }
-#include <stdio.h>
-#include <time.h>
+int helper(int n) {
+    if (n > 5) return 0;
+    cout << n << " ";
+    return helper(n + 1); 
+}
 
 int main() {
-    clock_t start = clock();
-
-    // Some heavy work
-    for (long long i = 0; i < 100000000; i++);
-
-    clock_t end = clock();
-    double cpu_time = (double)(end - start) / CLOCKS_PER_SEC;
-
-    printf("CPU time used: %f seconds\n", cpu_time);
-    return 0;
+    helper(1);
 }
