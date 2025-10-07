b1637b3fb49acbaef6a1ea3b6133845f7ed63c20
Avinash <avinash592004@gmail.com>
2025-10-07 16:53:36 +0530

    iterative

--- playground.cpp
+++ playground.cpp
@@ -1,13 +1,22 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
-int fb(int n){
-if(n==1 || n==0){
-    return n;
-}
-return fb(n-1)+fb(n-2);
-}
 
 int main(){
-cout << fb(0);
+    vector<int> nums = {1,2,3};
+    
+    vector<vector<int>> sub {{1}};
+    for(int num : nums){
+        int n = sub.size();
+        // for(int i; i<n; i++){
+        //     cout << sub[i];
+        // }
+        for ( auto v : sub){
+            for( int i : v){
+                cout << i << endl;
+            }
+        }
+    }
+
+
 }
