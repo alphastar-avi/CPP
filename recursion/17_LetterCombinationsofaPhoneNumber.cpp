be4381fcf098d2c95dd771755092960e4757ba05
Avinash <avinash592004@gmail.com>
2025-11-01 23:59:52 +0530

    17. Letter Combinations of a Phone Number

--- recursion/17_LetterCombinationsofaPhoneNumber.cpp
+++ recursion/17_LetterCombinationsofaPhoneNumber.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    vector<string> res;
+    unordered_map<char, string> digitToChar = {
+        {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+        {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+        {'8', "tuv"}, {'9', "wxyz"}
+    };
+
+    void backtrack(int i, string curStr, string &digits) {
+        if (curStr.length() == digits.length()) {
+            res.push_back(curStr);
+            return;
+        }
+
+        for (char c : digitToChar[digits[i]]) {
+            backtrack(i + 1, curStr + c, digits);
+        }
+    }
+
     vector<string> letterCombinations(string digits) {
-        vector<string> res;
-        if (digits.empty()) return res;
-
-        unordered_map<char, string> digitToChar = {
-            {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
-            {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
-            {'8', "tuv"}, {'9', "wxyz"}
-        };
-
-        function<void(int, string)> backtrack = [&](int i, string curStr) {
-            if (curStr.size() == digits.size()) {
-                res.push_back(curStr);
-                return;
-            }
-
-            for (char c : digitToChar[digits[i]]) {
-                backtrack(i + 1, curStr + c);
-            }
-        };
-
-        backtrack(0, "");
+        res.clear();
+        if (!digits.empty()) {
+            backtrack(0, "", digits);
+        }
         return res;
     }
 };
