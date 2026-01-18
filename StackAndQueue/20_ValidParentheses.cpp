71cfb684a5b46a01faadeb0ecb108ef4dc98e762
Avinash <avinash592004@gmail.com>
2026-01-18 21:52:52 +0530

    20. Valid Parentheses

--- StackAndQueue/20_ValidParentheses.cpp
+++ StackAndQueue/20_ValidParentheses.cpp
@@ -22,3 +22,28 @@ public:
         return ros.empty();
     }
 };
+//OR
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
