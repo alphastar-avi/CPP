346f3eaf5ec2fe0ec7917917b292fe435f11bac7
Avinash <avinash592004@gmail.com>
2026-01-19 01:06:40 +0530

    155. Min Stack

---
+++ StackAndQueue/155_MinStack.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+public:
+    stack<pair<int,int>> ros;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+       if(ros.empty()){
+        ros.push({val,val});
+       } else {
+        ros.push({val,min(val,ros.top().second)});
+       }
+        
+    }
+    
+    void pop() {
+        ros.pop();
+    }
+    
+    int top() {
+        return ros.top().first;
+       
+    }
+    
+    int getMin() {
+        return ros.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
