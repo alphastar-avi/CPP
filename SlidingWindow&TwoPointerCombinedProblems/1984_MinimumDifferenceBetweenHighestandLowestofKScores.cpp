85d427a1bd444a9f4beb7c0a5515873a95814cb4
Avinash <avinash592004@gmail.com>
2026-01-31 00:01:32 +0530

    fix

--- SlidingWindow&TwoPointerCombinedProblems/1984_MinimumDifferenceBetweenHighestandLowestofKScores.cpp
+++ SlidingWindow&TwoPointerCombinedProblems/1984_MinimumDifferenceBetweenHighestandLowestofKScores.cpp
@@ -3,8 +3,10 @@ public:
     int minimumDifference(vector<int>& nums, int k) {
         int n = nums.size();
         if(n <= 1) return 0;
+        
         sort(nums.begin(), nums.end());
         int minScore = nums[k-1] - nums[0];
+
         for(int i = 0; i <= n - k; i++){
             minScore = min(minScore, nums[i + k - 1] - nums[i]);
         }
