2ec7d48f83d819729a6f41676120c595a846144d
Avinash <avinash592004@gmail.com>
2026-02-19 23:48:49 +0530

    696. Count Binary Substrings 2 sol

--- Strings/696_CountBinarySubstrings.cpp
+++ Strings/696_CountBinarySubstrings.cpp
@@ -14,3 +14,30 @@ public:
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     int countBinarySubstrings(string s) {
+//         vector<int> groups;
+//         int count = 1;
+
+//         // Step 1: Count consecutive groups
+//         for (int i = 1; i < s.size(); i++) {
+//             if (s[i] == s[i - 1]) {
+//                 count++;
+//             } else {
+//                 groups.push_back(count);
+//                 count = 1;
+//             }
+//         }
+//         groups.push_back(count);
+
+//         // Step 2: Sum min of adjacent groups
+//         int result = 0;
+//         for (int i = 1; i < groups.size(); i++) {
+//             result += min(groups[i - 1], groups[i]);
+//         }
+
+//         return result;
+//     }
+// };
