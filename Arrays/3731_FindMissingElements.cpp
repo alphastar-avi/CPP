6d5fe0b00d698d729b044d7cd4cdd7c42adc3a73
Avinash <avinash592004@gmail.com>
2026-08-09 20:14:59 +0530

    3731. Find Missing Elements

--- Arrays/3731_FindMissingElements.cpp
+++ Arrays/3731_FindMissingElements.cpp
@@ -16,3 +16,25 @@ public:
 };
 
 //optimal solution is below 
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int maxx = INT_MIN;
+        int minn = INT_MAX;
+
+        for(int i : nums){
+            maxx = max(maxx, i);
+            minn = min(minn, i);
+        }
+
+        unordered_set<int> seen(nums.begin(), nums.end());
+        vector<int> res;
+        
+        for(int i=minn; i<=maxx; i++){
+            if(!seen.count(i)){
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
