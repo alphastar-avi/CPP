09801136b7900c7ae1c84e8bf4ecd01520a861a6
Avinash <avinash592004@gmail.com>
2026-09-13 12:32:56 +0530

    revise 496. Next Greater Element I

--- StackAndQueue/monotonic_stack/496_NextGreaterElementI.cpp
+++ StackAndQueue/monotonic_stack/496_NextGreaterElementI.cpp
@@ -16,3 +16,25 @@ public:
     return nums1;
     }
 };
+
+//revise
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> mono;
+        map<int,int> mpp;
+
+        for(int i=nums2.size()-1; i>=0; i--){
+            mpp[nums2[i]] = -1;
+
+            while(!mono.empty() && mono.top()<=nums2[i]) mono.pop();
+            if(!mono.empty()) mpp[nums2[i]]=mono.top();
+            mono.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(auto x : nums1){
+            ans.push_back(mpp[x]);
+        }
+        return ans;
+    }
+};
